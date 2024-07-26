ed448245b2e94ad73c29ac8ff3928d26feb0f9e6
Iván Fuentes Mahíllo <169067125+IFM1979@users.noreply.github.com>
2024-07-26 07:55:55 +0000

    Proyecto Entregado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,49 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// Botón recarga la página al hacer click
+const reloadButton = document.getElementById("reloadButton");
+reloadButton.addEventListener("click", () => {
+  location.reload();
+});
+
+// Temporizador
+setInterval(function() {
+  location.reload(); // Recarga la página
+}, 10000); // 10000 milisegundos = 10 segundos
+
+let suitsCards = ["♦", "♥", "♠", "♣"];
+let numbersCards = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "K",
+  "Q",
+  "J"
+];
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let suitsCardsRamdom =
+    suitsCards[Math.floor(Math.random() * suitsCards.length)];
+  let numbersCardsRamdom =
+    numbersCards[Math.floor(Math.random() * numbersCards.length)];
+
+  let selectorSuits = document.querySelectorAll(".suits");
+  for (let element of selectorSuits) {
+    element.innerHTML = suitsCardsRamdom;
+    if (suitsCardsRamdom === "♦" || suitsCardsRamdom === "♥") {
+      element.style.color = "red";
+    }
+  }
+
+  document.querySelector(".number").innerHTML = numbersCardsRamdom;
 };
